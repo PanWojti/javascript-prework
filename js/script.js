4ab7898d6e77dcc4a43acf146c30fd801eb2421b
Wojciech Sitkiewicz <sitkiewicz.wojciech@gmail.com>
2019-08-07 19:38:55 +0200

    Add result of the game

--- js/script.js
+++ js/script.js
@@ -28,8 +28,30 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2') {
   playerMove = 'papier';
-} else {
+} else if (playerInput == '3') {
   playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+  printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('Remis!');
+} else if (playerMove == 'nieznany ruch') {
+  printMessage('Wprowadziłeś nieprawidłową cyfrę!');
+}
